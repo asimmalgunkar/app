41e9ba89aad1ec5032c157578d46e97248cf27da
asimmalgunkar <asimmalgunkar.2015@gmail.com>
2026-07-03 16:43:41 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // 1) Create the Google Apps Script using google_apps_script_code.gs.
 // 2) Deploy it as Web App.
 // 3) Paste the Web App URL below.
-https://script.google.com/macros/s/AKfycbyJkB1BxreeMPPmg7bQkjKByv75phIFhdYlK-fO8_7zt8DfqugvEfKPmOl-rEOTREkFeg/exec
+const CONFIG = {
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyJkB1BxreeMPPmg7bQkjKByv75phIFhdYlK-fO8_7zt8DfqugvEfKPmOl-rEOTREkFeg/exec" }

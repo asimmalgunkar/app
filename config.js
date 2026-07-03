8806a0a4eef64a6fffb8dbc745a8b8fe3969ed23
asimmalgunkar <asimmalgunkar.2015@gmail.com>
2026-07-03 16:21:55 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// 1) Create the Google Apps Script using google_apps_script_code.gs.
+// 2) Deploy it as Web App.
+// 3) Paste the Web App URL below.
+const GOOGLE_SCRIPT_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";

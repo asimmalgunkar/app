a710ae9b55b9b2e93a92fbebcd2565d5295e523b
asimmalgunkar <asimmalgunkar.2015@gmail.com>
2026-07-03 16:56:35 +0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// 1) Create the Google Apps Script using google_apps_script_code.gs.
-// 2) Deploy it as Web App.
-// 3) Paste the Web App URL below.
-const GOOGLE_SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbjBkB1BxreeMPmg7bQkjKByv75phIFhdY1K-fO8_7zt8DfqquvEfKPmOl-rEOTRekFeg/exec";

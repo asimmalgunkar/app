774c7772c05a949d80296ff99869075aaa9d590b
asimmalgunkar <asimmalgunkar.2015@gmail.com>
2026-07-03 17:19:21 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Google Apps Script Web App URL
 // This file supports both versions of the website code.
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbjBkB1BxreeMPPMG7bQkjKByv75phIFhdY1K-fO8_7zt8DfqquvEfKPmOl-rEOTRekFeg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyJkB1BxreeMPPmg7bQkjKByv75phIFhdYlK-fO8_7zt8DfqugvEfKPmOl-rEOTREkFeg/exec";
 
 const CONFIG = {
   WEB_APP_URL: GOOGLE_SCRIPT_URL
